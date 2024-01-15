fd95d44b65014ce5289d0c4b7c6e6ffc2a31d933
Damian <damianprakash07@gmail.com>
2024-01-15 18:30:35 +0530

    edited README.md file

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -131,7 +131,7 @@ const SignIn = () => {
     <Container>
       <Wrapper>
         <Title>Sign in</Title>
-        <SubTitle>to continue to LamaTube</SubTitle>
+        <SubTitle>to continue to VidVortex</SubTitle>
         <Input
           placeholder="username"
           onChange={(e) => setName(e.target.value)}
